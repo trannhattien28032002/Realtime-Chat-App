0d94cc40a53e38ca976c750a8ca1660b7c6b7490
Tyler Tran <trannhattien28032002@gmail.com>
2025-02-17 21:03:41 +0700

    UI Design: Sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Home from './pages/home/Home';
 import Login from "./pages/login/Login";
 import SignUp from "./pages/signup/SignUp";
 
@@ -6,7 +7,8 @@ function App() {
   return (
     <>
       <div className="p-4 h-screen flex items-center justify-center">
-        <SignUp />
+        {/* <SignUp /> */}
+        <Home />
       </div>
     </>
   )
