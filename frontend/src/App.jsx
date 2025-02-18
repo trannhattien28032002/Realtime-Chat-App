c82539070ec3185f1bf5fe565bf72b355c2d8de5
Tyler Tran <trannhattien28032002@gmail.com>
2025-02-18 15:44:57 +0700

    SignUp Functionality/LogOut Functionality, AuthContext

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,22 @@
-import './App.css'
-import MessageContainer from './components/messages/MessageContainer';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Login from "./pages/login/Login";
 import SignUp from "./pages/signup/SignUp";
+import './App.css'
+import { useAuthContext } from './context/AuthContext';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
+  const { authUser } = useAuthContext();
   return (
     <>
       <div className="p-4 h-screen flex items-center justify-center">
-        {/* <SignUp /> */}
-        <Home />
+        <Routes>
+          <Route path='/' element={authUser ? <Home/> : <Navigate to="/login" />} />
+          <Route path='/login' element={authUser ? <Navigate to="/" /> : <Login />} />
+          <Route path='/signup' element={authUser ? <Navigate to="/" /> : <SignUp /> } />
+        </Routes>
+        <Toaster />
       </div>
     </>
   )
